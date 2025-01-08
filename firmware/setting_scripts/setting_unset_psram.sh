8d8832838dc956c094070de58c5f984e5304046d
KuraZuzu <kurasawa@rt-net.jp>
2025-01-08 15:01:34 +0900

    PSRAM無効化のsedコマンドのバリエーションにmac向けの条件を追加

--- firmware/setting_scripts/setting_unset_psram.sh
+++ firmware/setting_scripts/setting_unset_psram.sh
@@ -3,6 +3,14 @@
 # 設定ファイルパスを取得
 SDKCONFIG="$HOME/.local/share/moddable/build/devices/esp32/targets/m5stack_cores3/sdkconfig/sdkconfig.defaults"
 
-# PSRAMを無効化
-sed -i 's/CONFIG_SPIRAM=y/CONFIG_SPIRAM=n/' $SDKCONFIG
-cat $SDKCONFIG
+# macOS と Linux の sed コマンドの互換性を考慮
+if [ "$(uname)" = "Darwin" ]; then
+    # macOS 用
+    sed -i '' 's/CONFIG_SPIRAM=y/CONFIG_SPIRAM=n/' "$SDKCONFIG"
+else
+    # Linux 用
+    sed -i 's/CONFIG_SPIRAM=y/CONFIG_SPIRAM=n/' "$SDKCONFIG"
+fi
+
+# 結果を表示
+cat "$SDKCONFIG"
